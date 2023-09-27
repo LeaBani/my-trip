9fc0f2b5e961f41dcbae63cd4baa3160b4115605
LeaBani <113905453+LeaBani@users.noreply.github.com>
2023-09-27 12:27:08 +0200

    add home page and navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
 export default function Home() {
   return (
     <main>
-      <h1>Plan my trip</h1>
-      <h2>By train</h2>
+      <div id="#accueil" className="flex flex-col justify-center items-center sm:text-sm">
+        <h1 className=" text-2xl mb-5 text-neutral-500 ">Bienvenue sur My trip ! </h1>
+          <p className=" mb-5 w-4/5 lg:text-lg text-neutral-500">Ici tu trouveras mon carnet de voyagge.</p>
+          <p className="lg:text-lg  text-neutral-500 mb-3 sm:w-4/5">Si tu souhaites participer au projet, nhésite pas à me contacter sur mes réseaux!</p>
+      </div>
     </main>
   )
 }
